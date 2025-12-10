cd44d739f2c346ce0eb9cf8b36a37fde42e388f3
Aziz Kanoun <azizk@localhost.localdomain>
2025-12-10 20:14:20 +0100

    K3S Integration Test

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: fs.existsSync('/etc/ssl/private/ssl-cert-snakeoil.key') && fs.existsSync('/etc/ssl/certs/ssl-cert-snakeoil.pem') ? {
+      key: fs.readFileSync('/etc/ssl/private/ssl-cert-snakeoil.key'),
+      cert: fs.readFileSync('/etc/ssl/certs/ssl-cert-snakeoil.pem')
+    } : undefined
+  }
 })
